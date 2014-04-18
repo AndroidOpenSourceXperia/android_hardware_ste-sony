352a7007c2865e8a46423fdbade2c3fedee362e7
DevSwift <devswift@devswift-R520-R522-R620.(none)>
2014-04-18 21:46:39 +0200

    update

---
+++ common.mk
@@ -0,0 +1,27 @@
+# Audio
+PRODUCT_PACKAGES += \
+   libasound
+
+# STE Display 
+PRODUCT_PACKAGES += \
+   lights.montblanc \
+   gralloc.montblanc \
+   copybit.montblanc \
+   libblt_hw
+
+# STE Media
+PRODUCT_PACKAGES += \
+   libomxil-bellagio \
+   libstelpcutils
+
+# WLAN
+PRODUCT_PACKAGES += \
+   libnetcmdiface \
+   libnl_2 \
+   iw
+
+# External
+PRODUCT_PACKAGES += \
+   libhealthd
+
+
